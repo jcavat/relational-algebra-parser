421b61a92310f515673a970b4829f64dee067d27
Joel Cavat <jcavat@gmail.com>
2019-04-11 15:37:13 +0200

    Refactore AST

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -5,24 +5,43 @@ package example
 object Hello extends App {
   import fastparse._, NoWhitespace._
 
-  case class Identifier(name: String)
-
   object Ast {
+
+    case class RelationalId(name: String)
+    case class AttributeId(name: String)
+
+    sealed trait Op
+    object Op {
+      case object Eq extends Op
+    }
+
     sealed trait Relation
     object Relation {
-      case class SingleRelation(name: Identifier) extends Relation
-      case class Join(left: SingleRelation, right: SingleRelation) extends Relation
+      case class SingleRelation(name: RelationalId) extends Relation
+      case class Join(left: SingleRelation, cond: Cond, right: SingleRelation) extends Relation
+      case class Cond(left: String, op: Op, right: String) extends Relation
     }
   }
-  def argName[_: P] = CharIn("a-z").rep(1)
-  def relName[_: P] = CharIn("A-Z") ~ CharIn("a-z").rep
-  def arguments[_: P] = P( " ".rep ~ argName.!.rep(sep=" ".rep ~ "," ~ " ".rep./) )
-  def relation[_: P] = P(relName.!.map( n => Ast.Relation.SingleRelation(Identifier(n))))
-  def join[_: P] = P(relation ~ " join " ~ relation).map( n => Ast.Relation.Join(n._1, n._2))
-  def relExpr[_: P] = P("(" ~ (join|relation) ~ ")")
-  def piArguments[_: P] = P("pi(" ~ arguments ~ ")" ~ relExpr)
 
-  val Parsed.Success(value, successIndex) = parse("pi(test, coucou)(Person join Voiture)", piArguments(_))
+  import Ast._
+
+  def idName[_: P] = CharIn("a-z").rep(1)
+  def capitalizedIdName[_: P] = CharIn("A-Z") ~ CharIn("a-z").rep
+
+  def attributeName[_: P] = P( idName.! ).map( AttributeId(_) )
+  def relationName[_: P] = P( capitalizedIdName.! ).map( n => Ast.Relation.SingleRelation(RelationalId(n)))
+
+  def funcArguments[_: P] = P( " ".rep ~ attributeName.rep(sep=" ".rep ~ "," ~ " ".rep./) )
+
+  def join[_: P] = ???
+  def joinExpr[_: P] = P(relationName ~ " joinExpr " ~ eqJoinCond ~ relationName).map( n => Ast.Relation.Join(n._1, n._2, n._3))
+  def eqJoinCond[_: P] = P("on (" ~ idName.! ~ " = " ~ idName.! ~ ") ").map( n => Ast.Relation.Cond(n._1, Op.Eq, n._2) )
+
+
+  def relExpr[_: P] = P("(" ~ (joinExpr|relationName) ~ ")")
+  def piArguments[_: P] = P("pi(" ~ funcArguments ~ ")" ~ relExpr)
+
+  val Parsed.Success(value, successIndex) = parse("pi(test, coucou)(Person joinExpr on (u = b) Voiture)", piArguments(_))
   
   println(value)
 }
