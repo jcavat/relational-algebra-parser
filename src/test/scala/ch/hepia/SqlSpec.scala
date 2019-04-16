b75dd2f4a72b80d83a8905826e7ae90c95313ce7
Joel Cavat <jcavat@gmail.com>
2019-04-16 15:51:13 +0200

    Add test for SQL output

---
+++ src/test/scala/ch/hepia/SqlSpec.scala
@@ -0,0 +1,64 @@
+package ch.hepia
+
+import ch.hepia.Parser.parseAlgebra
+import fastparse.{Parsed, _}
+import org.scalatest._
+
+class SqlSpec extends FlatSpec with Matchers {
+  import ch.hepia.sql.ShowSql._
+  import ch.hepia.sql.AstShowSql._
+  "One relation" should "export to SQL successfully" in {
+    val Parsed.Success(value, _) = parse("Person", parseAlgebra(_))
+    value.showSql should equal(
+      """
+        |SELECT *
+        |FROM Person
+      """.stripMargin.stripLeading.stripTrailing)
+  }
+  "One join" should "export to SQL successfully" in {
+    val Parsed.Success(value, _) = parse("Person join(p = c) Car", parseAlgebra(_))
+    value.showSql should equal(
+      """
+        |SELECT *
+        |FROM Person
+        |INNER JOIN Car ON p = c
+      """.stripMargin.stripLeading.stripTrailing)
+  }
+  "Pi expr with one relation" should "export to SQL successfully" in {
+    val Parsed.Success(value, _) = parse("pi(firstname, lastname)(Person)", parseAlgebra(_))
+    value.showSql should equal(
+      """
+        |SELECT firstname, lastname
+        |FROM Person
+      """.stripMargin.stripLeading.stripTrailing)
+  }
+  "Pi expr with one join" should "export to SQL successfully" in {
+    val Parsed.Success(value, _) = parse("pi(firstname, lastname)(Person join(p = c) Car)", parseAlgebra(_))
+    value.showSql should equal(
+      """
+        |SELECT firstname, lastname
+        |FROM Person
+        |INNER JOIN Car ON p = c
+      """.stripMargin.stripLeading.stripTrailing)
+  }
+  "Sigma expr with one join" should "export to SQL successfully" in {
+    val Parsed.Success(value, _) = parse("sigma(age = 30)(Person join(p = c) Car)", parseAlgebra(_))
+    value.showSql should equal(
+      """
+        |SELECT *
+        |FROM Person
+        |INNER JOIN Car ON p = c
+        |WHERE age = 30
+      """.stripMargin.stripLeading.stripTrailing)
+  }
+  "Pi/Sigma expr with one join" should "export to SQL successfully" in {
+    val Parsed.Success(value, _) = parse("pi(firstname, age)(sigma(age > 18)(Person join(p = c) Car))", parseAlgebra(_))
+    value.showSql should equal(
+      """
+        |SELECT firstname, age
+        |FROM Person
+        |INNER JOIN Car ON p = c
+        |WHERE age > 18
+      """.stripMargin.stripLeading.stripTrailing)
+  }
+}
